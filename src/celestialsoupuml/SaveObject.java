95cd5e2be367036301e998b3f3d4a2acc4174cc4
Brandan Jablonski <bwjablon@gmail.com>
2016-11-07 11:28:30 -0500

    Made it clear as to why this exists

--- src/celestialsoupuml/SaveObject.java
+++ src/celestialsoupuml/SaveObject.java
@@ -9,6 +9,11 @@ package celestialsoupuml;
  *
  * @author Brandan
  */
+
+/*
+   This class exists because ShapeContainer cannot be serialized properly
+   due to there being a super class in it.
+*/
 public class SaveObject implements java.io.Serializable{
     
     public ShapeEnum type;
